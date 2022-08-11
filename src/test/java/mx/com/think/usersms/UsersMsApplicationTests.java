707c474bbf44e6ce4775e501b464c3b73346f4f0
Julio César <ing.juliocesar26@outlook.com>
2022-08-10 23:16:33 -0500

    build(microservice): Create Users microservice
    Creation and initial configuration for Users Microservice
    
    Author: Jromero@cxc.com.mx

---
+++ src/test/java/mx/com/think/usersms/UsersMsApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.think.usersms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
